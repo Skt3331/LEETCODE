e1c7631b617947576b3ba0600850f6a79390d77c
Saurav Temgire <144504510+Skt3331@users.noreply.github.com>
2024-02-24 21:59:44 +0530

    Time: 9 ms (95.03%), Space: 56.7 MB (54.91%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+     if (head == null || head.next == null)
+            return head;
+
+        // Find the middle of the linked list
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Split the linked list into two halves
+        prev.next = null;
+
+        // Recursively sort the two halves
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+
+        // Merge the sorted halves
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        if (l1 != null)
+            current.next = l1;
+        else
+            current.next = l2;
+
+        return dummy.next;
+    }
+
+    // Helper method to print the linked list
+    public void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
