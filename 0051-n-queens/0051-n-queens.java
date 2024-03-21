55f283ce16a56546c68b340d568e593fb258c5b8
Saurav Temgire <144504510+Skt3331@users.noreply.github.com>
2024-03-21 16:33:33 +0530

    Time: 2 ms (87.78%), Space: 44.4 MB (91.90%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(board[i], '.');
+        }
+        solveNQueensHelper(board, 0, result);
+        return result;
+    }
+    
+    private void solveNQueensHelper(char[][] board, int row, List<List<String>> result) {
+        if (row == board.length) {
+            result.add(constructSolution(board));
+            return;
+        }
+        
+        for (int col = 0; col < board.length; col++) {
+            if (isValid(board, row, col)) {
+                board[row][col] = 'Q';
+                solveNQueensHelper(board, row + 1, result);
+                board[row][col] = '.';
+            }
+        }
+    }
+    
+    private boolean isValid(char[][] board, int row, int col) {
+        // Check column
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        
+        // Check upper left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        // Check upper right diagonal
+        for (int i = row, j = col; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private List<String> constructSolution(char[][] board) {
+        List<String> solution = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            solution.add(new String(board[i]));
+        }
+        return solution;
+    }
+}
