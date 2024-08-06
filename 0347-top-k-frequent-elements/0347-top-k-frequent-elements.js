8b2d624b984fc5bd93ef71b5da07d93151545213
Saurav Temgire <144504510+Skt3331@users.noreply.github.com>
2024-08-06 18:59:02 +0530

    Time: 68 ms (63.38%), Space: 54.2 MB (48.88%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let map={};
+    let bucket=[];
+    let result=[];
+    
+    for(let i=0;i<nums.length;i++)
+        {
+            if(!map[nums[i]])
+                {
+                    map[nums[i]]=1;
+                }
+            else
+                {
+                    map[nums[i]]++;
+                }
+        }
+    for(let [num,freq] of Object.entries(map))
+        {
+        if(!bucket[freq]){
+            bucket[freq]=new Set().add(num)
+        }
+        else
+        {
+            bucket[freq]=bucket[freq].add(num)
+        }
+        }
+    for(let i=bucket.length-1;i>=0;i--)
+        {
+            if(bucket[i]) result.push(...bucket[i]);
+            if(result.length===k) break;
+        }
+    return result;
+    
+};
