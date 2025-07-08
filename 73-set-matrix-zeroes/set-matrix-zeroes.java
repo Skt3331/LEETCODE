d9c09d7a3fdbea0f4f4bf91767122751679aacee
Saurav Temgire <144504510+Skt3331@users.noreply.github.com>
2025-07-08 21:40:35 +0530

    Time: 1 ms (72.34%) | Memory: 45.8 MB (27.62%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,28 +2,34 @@ class Solution {
 
 
   public void setZeroes(int[][] matrix) {
-    int m = matrix.length, n = matrix[0].length;
-    boolean[] row = new boolean[m];
-    boolean[] col = new boolean[n];
+    
+    int m=matrix.length,n=matrix[0].length;
+    boolean[] row=new boolean[m];
+    boolean[] col=new boolean[n];
 
-    // Mark the rows and columns that should be zeroed
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] == 0) {
-                row[i] = true;
-                col[j] = true;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(matrix[i][j]==0)
+            {
+                row[i]=true;
+                col[j]=true;
             }
         }
     }
 
-    // Set zeroes
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (row[i] || col[j]) {
-                matrix[i][j] = 0;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(row[i]||col[j])
+            {
+              matrix[i][j]=0;
             }
         }
     }
-}
+
+  }
 
 }
