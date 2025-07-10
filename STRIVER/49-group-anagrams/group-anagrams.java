697e4a09ebe47f5047739cd5aad4ccc51a54f6ae
Saurav Temgire <144504510+Skt3331@users.noreply.github.com>
2025-07-10 21:12:22 +0530

    Time: 17 ms (26.78%) | Memory: 48.2 MB (28.71%) - LeetSync

--- STRIVER/49-group-anagrams/group-anagrams.java
+++ STRIVER/49-group-anagrams/group-anagrams.java
@@ -1,13 +1,33 @@
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> hmap = new HashMap<>();
-        for (String val : strs) {
-            int[] shash = new int[26];
-            for (char c : val.toCharArray()) shash[c - 'a']++;
+        for (String s : strs) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) count[c - 'a']++;
             StringBuilder key = new StringBuilder();
-            for (int n : shash) key.append('#').append(n);
-            hmap.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(val);
+            for (int i = 0; i < 26; i++) {
+                key.append('#').append(count[i]);
+            }
+            hmap.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(s);
         }
         return new ArrayList<>(hmap.values());
     }
 }
+
+
+
+
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         Map<String, List<String>> hmap = new HashMap<>();
+//         for (String val : strs) {
+//             int[] shash = new int[26];
+//             for (char c : val.toCharArray()) shash[c - 'a']++;
+//             StringBuilder key = new StringBuilder();
+//             for (int n : shash) key.append('#').append(n);
+//             hmap.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(val);
+//         }
+//         return new ArrayList<>(hmap.values());
+//     }
+// }
