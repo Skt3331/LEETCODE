7f76f04fde0604033083827f3edacfd691f56662
Saurav Temgire <144504510+Skt3331@users.noreply.github.com>
2025-07-10 20:30:13 +0530

    Time: 25 ms (6.77%) | Memory: 59.1 MB (27.28%) - LeetSync

--- STRIVER/217-contains-duplicate/contains-duplicate.java
+++ STRIVER/217-contains-duplicate/contains-duplicate.java
@@ -1,16 +1,20 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        
-        Set<Integer> hash=new HashSet<>();
-        for(int i:nums)
-        {
-            if(hash.contains(i))
-            {
-                return true;
-            }
-            hash.add(i);
+   public boolean containsDuplicate(int[] nums) {
+    Set<Integer> hash = Arrays.stream(nums)
+                              .boxed()
+                              .collect(Collectors.toSet());
+    return hash.size() != nums.length;
 
-        }
-        return false;
+        // Set<Integer> hash=new HashSet<>();
+        // for(int i:nums)
+        // {
+        //     if(hash.contains(i))
+        //     {
+        //         return true;
+        //     }
+        //     hash.add(i);
+
+        // }
+        // return false;
     }
 }
