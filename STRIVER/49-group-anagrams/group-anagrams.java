41ddd2ca6ecf0444c2eeb17ab45b3193bd1e6d49
Saurav Temgire <144504510+Skt3331@users.noreply.github.com>
2025-07-10 21:13:26 +0530

    Time: 17 ms (26.78%) | Memory: 48.1 MB (44.59%) - LeetSync

--- STRIVER/49-group-anagrams/group-anagrams.java
+++ STRIVER/49-group-anagrams/group-anagrams.java
@@ -1,17 +1,20 @@
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> hmap = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
             int[] count = new int[26];
-            for (char c : s.toCharArray()) count[c - 'a']++;
-            StringBuilder key = new StringBuilder();
+            for (int i = 0; i < s.length(); i++)
+                count[s.charAt(i) - 'a']++;
+            // Build a key like "#1#0#0#0#0..." to uniquely identify anagrams
+            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < 26; i++) {
-                key.append('#').append(count[i]);
+                sb.append('#');
+                sb.append(count[i]);
             }
-            hmap.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(s);
+            String key = sb.toString();
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
-        return new ArrayList<>(hmap.values());
+        return new ArrayList<>(map.values());
     }
 }
 
