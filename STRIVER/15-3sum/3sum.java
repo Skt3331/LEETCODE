d34ef2e9b0b151a2d470f02a22f345c6262349d2
Saurav Temgire <144504510+Skt3331@users.noreply.github.com>
2025-07-14 21:53:11 +0530

    Time: 30 ms (69.68%) | Memory: 51.4 MB (83.28%) - LeetSync

--- STRIVER/15-3sum/3sum.java
+++ STRIVER/15-3sum/3sum.java
@@ -6,33 +6,31 @@ public class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate values for i
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            twoPointer(nums, i, res);
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            twosum(i, nums, res);
         }
         return res;
     }
 
-    // Two-pointer helper function
-    private void twoPointer(int[] nums, int i, List<List<Integer>> res) {
+    public void twosum(int i, int[] nums, List<List<Integer>> res) {
         int left = i + 1;
         int right = nums.length - 1;
 
         while (left < right) {
             int sum = nums[i] + nums[left] + nums[right];
-
             if (sum == 0) {
                 res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                // Skip duplicates for left
+
                 while (left < right && nums[left] == nums[left + 1]) left++;
-                // Skip duplicates for right
                 while (left < right && nums[right] == nums[right - 1]) right--;
+
                 left++;
                 right--;
-            } else if (sum < 0) {
-                left++;
+            } else if (sum > 0) {
+                right--;
             } else {
-                right--;
+                left++;
             }
         }
     }
